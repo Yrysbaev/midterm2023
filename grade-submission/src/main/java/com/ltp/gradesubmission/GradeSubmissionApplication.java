cb1f030f32b765b7155a5c0fda498d6fd0a552b5
Yrysbaev <mzamirbekovic02@gmail.com>
2023-03-28 20:16:27 +0600

    Ready

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.ltp.gradesubmission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GradeSubmissionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GradeSubmissionApplication.class, args);
 	}
 
 }
